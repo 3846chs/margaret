51f872b03b49247766a6991f414532e0b5329d86
최현성 <choi@choihyeonseong-ui-MacBookPro.local>
2020-01-28 11:12:59 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:dating_app/constants/material_white_color.dart';
+import 'package:dating_app/main_page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainPage(),
+      theme: ThemeData(
+          primarySwatch: white
+      ),
+    );
+  }
+}
+
+
---
+++ lib/main_page.dart
@@ -0,0 +1,117 @@
+import 'package:dating_app/screens/chat_page.dart';
+import 'package:dating_app/screens/match_page.dart';
+import 'package:dating_app/screens/receive_page.dart';
+import 'package:dating_app/screens/send_page.dart';
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int _selectedIndex = 0;
+
+  static List<Widget> _widgetOptions = <Widget>[
+    MatchPage(),
+    SendPage(),
+    ReceivePage(),
+    ChatPage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('소개팅 앱'),
+      ),
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: _widgetOptions,
+      ),
+      drawer: _buildDrawer(),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.grey,
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Color.fromRGBO(249, 249, 249, 1),
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.group,
+              size: 30,
+            ),
+            title: Text(''),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.call_made,
+              size: 30,
+            ),
+            title: Text(''),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.call_received,
+              size: 30,
+            ),
+            title: Text(''),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.chat,
+              size: 30,
+            ),
+            title: Text(''),
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: (index) => _onItemTapped(index),
+      ),
+    );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+}
+
+class _buildDrawer extends StatelessWidget {
+  const _buildDrawer({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      // Add a ListView to the drawer. This ensures the user can scroll
+      // through the options in the drawer if there isn't enough vertical
+      // space to fit everything.
+      child: ListView(
+        // Important: Remove any padding from the ListView.
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: CircleAvatar(
+                // .....
+                ),
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+          ),
+          ListTile(title: Text('공지사항'), onTap: null),
+          ListTile(
+            title: Text('설정'),
+            onTap: null,
+          ),
+          ListTile(
+            title: Text('로그아웃'),
+            onTap: null,
+          ),
+        ],
+      ),
+    );
+  }
+}
