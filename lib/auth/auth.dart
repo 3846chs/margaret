a68e3427900fd645315b46b65dedfaf99fe5f6e9
ChoiHyeonSeong <3846chs@naver.com>
2020-02-20 23:57:07 +0900

    로그인 UI

--- lib/auth/auth.dart
+++ lib/auth/auth.dart
@@ -1,6 +1,9 @@
 import 'package:datingapp/auth/email/email_auth.dart';
+import 'package:datingapp/auth/login_button.dart';
+import 'package:datingapp/utils/base_height.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Auth extends StatelessWidget {
   @override
@@ -11,13 +14,27 @@ class Auth extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.end,
             children: <Widget>[
-              SignInButton(
-                Buttons.Facebook, // 페이스북 로그인
-                onPressed: () {},
+              LoginButton(
+                text: "구글로 로그인",
+                icon: FontAwesomeIcons.google,
+                color: Colors.red[600],
+                onPressed: () => null,
               ),
-              SizedBox(
-                height: 10,
+              SizedBox(height: screenAwareSize(5.0, context)),
+              LoginButton(
+                text: "페이스북으로 로그인",
+                icon: FontAwesomeIcons.facebookF,
+                color: Color(0xff3a5c93),
+                onPressed: () => null,
               ),
+              SizedBox(height: screenAwareSize(5.0, context)),
+              LoginButton(
+                text: "이메일로 로그인",
+                icon: FontAwesomeIcons.solidEnvelope,
+                color: Colors.grey[300],
+                onPressed: () => null,
+              ),
+              SizedBox(height: screenAwareSize(5.0, context)),
               SignInButton(
                 Buttons.Email, // 이메일 로그인
                 onPressed: () {
