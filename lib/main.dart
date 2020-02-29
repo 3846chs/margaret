5cdd6c68895f9321f82571f64270b9d7d68a993f
박현우 <ghwhsbsb123@gmail.com>
2020-03-01 01:30:52 +0900

    Fix image_picker permission bug.

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:margaret/home.dart';
 import 'package:margaret/widgets/loading_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:provider/provider.dart';
 import 'auth/auth_main.dart';
 
@@ -14,39 +15,52 @@ void main() => runApp(ChangeNotifierProvider<MyUserData>(
 class OurApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false, // 우측상단에 debug 라는 빨간색 띠 없애기
-      home: Consumer<MyUserData>(
-        builder: (context, myUserData, child) {
-          switch (myUserData.status) {
-            case MyUserDataStatus.progress:
-              FirebaseAuth.instance.currentUser().then((firebaseUser) async {
-                if (firebaseUser == null)
-                  myUserData.setNewStatus(MyUserDataStatus.none);
-                else {
-                  print(firebaseUser.uid);
-                  final snapShot = await Firestore.instance
-                      .collection('Users')
-                      .document(firebaseUser.uid)
-                      .get();
-                  if (snapShot == null || !snapShot.exists) {
-                    // 해당 snapshot 이 존재하지 않을 때
-                    print('Not yet Registered - Auth Page');
-                    myUserData.setNewStatus(MyUserDataStatus.none);
-                  } else {
-                    myUserData.setUserData(firebaseUser.uid);
-                  }
+    return FutureBuilder(
+        future:
+            PermissionHandler().requestPermissions([PermissionGroup.storage]),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState != ConnectionState.done)
+            return Material(
+              child: Center(
+                child: const CircularProgressIndicator(),
+              ),
+            );
+          return MaterialApp(
+            debugShowCheckedModeBanner: false, // 우측상단에 debug 라는 빨간색 띠 없애기
+            home: Consumer<MyUserData>(
+              builder: (context, myUserData, child) {
+                switch (myUserData.status) {
+                  case MyUserDataStatus.progress:
+                    FirebaseAuth.instance
+                        .currentUser()
+                        .then((firebaseUser) async {
+                      if (firebaseUser == null)
+                        myUserData.setNewStatus(MyUserDataStatus.none);
+                      else {
+                        print(firebaseUser.uid);
+                        final snapShot = await Firestore.instance
+                            .collection('Users')
+                            .document(firebaseUser.uid)
+                            .get();
+                        if (snapShot == null || !snapShot.exists) {
+                          // 해당 snapshot 이 존재하지 않을 때
+                          print('Not yet Registered - Auth Page');
+                          myUserData.setNewStatus(MyUserDataStatus.none);
+                        } else {
+                          myUserData.setUserData(firebaseUser.uid);
+                        }
+                      }
+                    });
+                    return LoadingPage();
+                  case MyUserDataStatus.exist:
+                    return Home();
+                  default:
+                    return AuthMain();
                 }
-              });
-              return LoadingPage();
-            case MyUserDataStatus.exist:
-              return Home();
-            default:
-              return AuthMain();
-          }
-        },
-      ),
-      theme: ThemeData(primarySwatch: white),
-    );
+              },
+            ),
+            theme: ThemeData(primarySwatch: white),
+          );
+        });
   }
 }
