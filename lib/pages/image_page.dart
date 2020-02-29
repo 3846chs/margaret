56ed342f7eb49c31c5b6f96ddcf0400683aa4735
박현우 <ghwhsbsb123@gmail.com>
2020-03-01 01:38:45 +0900

    Implement chat image zoom.

--- lib/pages/image_page.dart
+++ lib/pages/image_page.dart
@@ -1,6 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:margaret/firebase/storage_cache_manager.dart';
 import 'package:flutter/material.dart';
+import 'package:photo_view/photo_view.dart';
 
 class ImagePage extends StatelessWidget {
   final String src;
@@ -11,10 +12,12 @@ class ImagePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Center(
-        child: CachedNetworkImage(
-          imageUrl: src,
-          cacheManager: StorageCacheManager(),
+      body: Container(
+        child: PhotoView(
+          imageProvider: CachedNetworkImageProvider(
+            src,
+            cacheManager: StorageCacheManager(),
+          ),
         ),
       ),
     );
