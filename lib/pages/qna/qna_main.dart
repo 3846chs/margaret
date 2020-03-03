a0882e801668408ff8e2213528f546b0b21ff540
ChoiHyeonSeong <3846chs@naver.com>
2020-03-04 00:15:19 +0900

    Random QnA 탭(Tab 2) 알고리즘 구현

--- lib/pages/qna/qna_main.dart
+++ lib/pages/qna/qna_main.dart
@@ -1,8 +1,6 @@
-import 'package:margaret/constants/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:margaret/pages/qna/my_questions.dart';
 import 'package:margaret/pages/qna/peer_questions.dart';
-import 'package:margaret/pages/qna/write_question.dart';
 
 class QnAMain extends StatefulWidget {
   @override
@@ -16,19 +14,6 @@ class _QnAMainState extends State<QnAMain> {
       child: DefaultTabController(
         length: 2,
         child: Scaffold(
-          floatingActionButton: FloatingActionButton(
-            heroTag: 'QnAMain',
-            onPressed: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => WriteQuestion()));
-            },
-            backgroundColor: Colors.grey[100],
-            child: Icon(
-              Icons.edit,
-              color: pastel_purple,
-              size: 30,
-            ),
-          ),
           appBar: TabBar(
             indicatorColor: Colors.black54,
             tabs: <Widget>[
@@ -41,6 +26,7 @@ class _QnAMainState extends State<QnAMain> {
             ],
           ),
           body: TabBarView(
+//            physics: NeverScrollableScrollPhysics(),
             children: <Widget>[
               PeerQuestions(),
               MyQuestions(),
