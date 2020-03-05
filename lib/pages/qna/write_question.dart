a8134b1135d35d71c724e1aa15b8bf0dab3d052c
ChoiHyeonSeong <3846chs@naver.com>
2020-03-06 02:25:36 +0900

    Recover missing code

--- lib/pages/qna/write_question.dart
+++ lib/pages/qna/write_question.dart
@@ -1,7 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:margaret/constants/firebase_keys.dart';
 import 'package:margaret/constants/font_names.dart';
+import 'package:margaret/data/provider/my_user_data.dart';
 import 'package:margaret/pages/qna/radial_menu.dart';
 import 'package:margaret/utils/base_height.dart';
+import 'package:provider/provider.dart';
 
 class WriteQuestion extends StatefulWidget {
   @override
@@ -39,6 +43,40 @@ class _WriteQuestionState extends State<WriteQuestion> {
                 decoration: _buildInputDecoration('질문을 입력해주세요'),
                 maxLength: 100,
                 maxLines: 5),
+            Consumer<MyUserData>(builder: (context, myUserData, _) {
+              return FloatingActionButton(
+                  heroTag: 'WriteQuestion',
+                  backgroundColor: Colors.white,
+                  foregroundColor: Colors.black,
+                  child: Icon(
+                    Icons.send,
+                  ),
+                  onPressed: () async {
+                    String question = _questionController.text;
+                    String myUserKey = myUserData.userData.userKey;
+                    String timestamp =
+                    DateTime.now().millisecondsSinceEpoch.toString();
+                    _questionController.text = '';
+                    Navigator.pop(context);
+
+                    final QuerySnapshot querySnapshot = await Firestore.instance
+                        .collection(COLLECTION_USERS)
+                    //                        .orderBy('recentMatchTime', descending: true)
+                        .getDocuments();
+                    querySnapshot.documents
+                        .where((doc) => (doc['gender'] !=
+                        myUserData.userData.gender)) // 차단 유저 제외 -> 나중에
+                        .forEach((ds) {
+                      Firestore.instance
+                          .collection(COLLECTION_USERS)
+                          .document(ds.documentID)
+                          .collection('PeerQuestions')
+                          .document(timestamp)
+                          .setData(
+                          {'question': question, 'userKey': myUserKey});
+                    });
+                  });
+            }),
             SizedBox.fromSize(size: Size.fromHeight(300), child: RadialMenu()),
           ],
         )));
