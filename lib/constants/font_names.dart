f5c50299ee0630d3ff78c8926ce16f89e906cefa
ChoiHyeonSeong <3846chs@naver.com>
2020-03-13 16:33:43 +0900

    Add some fonts

--- lib/constants/font_names.dart
+++ lib/constants/font_names.dart
@@ -1,8 +1,10 @@
 class FontFamily{
   static const jua = 'BMJUA';
+  static const helloTwenty = 'HelloTwenty';
   static const handlee = 'handlee';
   static const pacifico = 'pacifico';
   static const nanumBarunpen = 'NanumBarunpen';
+  static const mapleStory = 'MapleStoryLight';
+  static const cookieRun = 'CookieRun';
   static const miSaeng = 'SDMiSaeng';
-
 }
