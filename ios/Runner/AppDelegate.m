bb6edb3c1fdcb43a2b849ec7fce5479466f766e4
ChoiHyeonSeong <3846chs@naver.com>
2020-03-15 15:18:22 +0900

    iOS 에뮬레이터 연결

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,17 +1,16 @@
 #import "AppDelegate.h"
 #import "GeneratedPluginRegistrant.h"
-#import <NaverThirdPartyLogin/NaverThirdPartyLogin.h>
+
+@import Firebase;
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  [FIRApp configure];
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
-
-- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<NSString *,id> *)options {
-    return [[NaverThirdPartyLoginConnection getSharedInstance] application:app openURL:url options:options];
 
 @end
